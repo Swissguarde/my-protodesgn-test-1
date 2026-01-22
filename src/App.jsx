e871c13a7da9157263838869caedaad0db8e89d7
Kolade Ayo <ayomikolade2003@gmail.com>
2026-01-22 09:30:10 +0100

    Update src/App.jsx from Node UI Editor

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <>
+      <div style={{"width":"auto","height":"auto","display":"block","padding":0,"margin":0,"background":"transparent"}}>Box
+      <p style={{"fontSize":16,"fontWeight":400,"lineHeight":1.4,"color":"#000"}}>Text</p>
+      </div>
+    </>
+  );
+}
